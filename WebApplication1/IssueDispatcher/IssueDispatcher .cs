e7337c6aff94e70731b85d22734f398c8e85729c
amb200 <56393401+amb200@users.noreply.github.com>
2023-08-15 15:53:30 +0300

    fix

--- WebApplication1/IssueDispatcher/IssueDispatcher .cs
+++ WebApplication1/IssueDispatcher/IssueDispatcher .cs
@@ -1,15 +1,13 @@
 ﻿using AutoMapper;
-using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
 using Polly;
 using System.Collections.Concurrent;
-using System.Text;
+using System.Net.Http.Formatting;
 using WebApplication1.Entities;
 using WebApplication1.Services;
 
 namespace WebApplication1.IssueDispatcher
 {
-    public class IssueDispatcher : IIssueDispatcher, IAsyncDisposable
+    public class IssueDispatcher : IIssueDispatcher
     {
         private readonly IServiceScopeFactory _serviceProvider;
         private readonly IConfiguration _configuration;
@@ -17,32 +15,28 @@ namespace WebApplication1.IssueDispatcher
         private readonly TimeSpan _retryInterval;
         private readonly IMapper _mapper;
 
+
         private readonly ConcurrentQueue<Issue> _issueQueue = new ConcurrentQueue<Issue>();
 
 
-        public IssueDispatcher(IServiceScopeFactory serviceProvider, IConfiguration configuration, IMapper mapper)
+        public IssueDispatcher(IServiceScopeFactory serviceProvider, IConfiguration configuration, IMapper mapper, IHostApplicationLifetime appLifetime)
         {
             _serviceProvider = serviceProvider;
             _configuration = configuration;
             _batchSize = _configuration.GetValue<int>("IssueDispatcher:BatchSize");
             _retryInterval = TimeSpan.FromSeconds(_configuration.GetValue<int>("IssueDispatcher:RetryIntervalSeconds"));
             _mapper = mapper;
+            appLifetime.ApplicationStopping.Register(OnApplicationStopping);
 
-            Task.Run(ProcessQueueAsync);
+            Task.Run(ProcessQueueAsync);//kill upon garbage collection
         }
 
-        public async ValueTask DisposeAsync()
+        private async void OnApplicationStopping()
         {
-            try
-            {
-                await SendIssuesToDatabaseAsync();
-            }
-            catch (Exception ex) {
-                IServiceScopeFactory b = _serviceProvider;
-                await SendIssuesToDatabaseAsync();
-            }
-            
+            await SendIssuesToDatabaseAsync();
         }
+
+
 
         public async void Send(Issue issue)
         {
@@ -64,34 +58,36 @@ namespace WebApplication1.IssueDispatcher
 
         private async Task SendIssuesToDatabaseAsync()
         {
-            using (var scope = _serviceProvider.CreateScope())
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:7264");
+            var token = HttpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");//fix
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            
+
+            var issuesToSend = new List<Issue>();
+            while (issuesToSend.Count < _batchSize && _issueQueue.TryDequeue(out var issue))
             {
-                var issueServices = scope.ServiceProvider.GetRequiredService<IIssueServices>();
-                var httpClientFactory = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>();
-                var httpClient = httpClientFactory.CreateClient();
-                httpClient.BaseAddress = new Uri("https://localhost:7264");
-
-                var issuesToSend = new List<Issue>();
-                while (issuesToSend.Count < _batchSize && _issueQueue.TryDequeue(out var issue))
-                {
-                    // Separate db input from user input 
-                    var created = _mapper.Map<Issue>(issue);
-                    created.EventId = 0;
-                    created.Timestamp = DateTime.UtcNow;
-                    issuesToSend.Add(created);
-                }
-
-                var policy = Policy
-                    .Handle<HttpRequestException>()
-                    .WaitAndRetryAsync(3, _ => _retryInterval);
-
-                await policy.ExecuteAsync(async () =>
-                {
-                    await issueServices.Add(issuesToSend);
-                });
+                // Separate db input from user input 
+                var created = _mapper.Map<Issue>(issue);
+                created.EventId = 0;
+                created.Timestamp = DateTime.UtcNow;
+                issuesToSend.Add(created);
             }
-        }
+
+            var populate = new HttpRequestMessage(HttpMethod.Post, "/api/issue");
+            populate.Content = new ObjectContent<List<Issue>>(issuesToSend, new JsonMediaTypeFormatter(), "application/json");
+
+            var policy = Policy
+                .Handle<HttpRequestException>()
+                .WaitAndRetryAsync(3, _ => _retryInterval);
+
+            await policy.ExecuteAsync(async () =>
+            {
+                await httpClient.SendAsync(populate);
+            });
+        
     }
+}
 }
 
 
